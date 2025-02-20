f107f537de6c6dabb0191046fb7aef075f140d67
Samgu Lee <cable8mm@gmail.com>
2025-02-21 02:10:00 +0900

    Write comments of classes all (#19)
    * Write comments of classes all
    
    * Fix doctum code for publishing API documents
    
    * Revise `README`

--- src/Enums/GoodCodeType.php
+++ src/Enums/GoodCodeType.php
@@ -10,6 +10,13 @@ enum GoodCodeType
     case GIFT;
     case OPTION;
 
+    /**
+     * Get prefix of GoodCodeType.
+     *
+     * @return string The method returns the prefix of the GoodCodeType
+     *
+     * @example GoodCodeType::GIFT->prefix() => 'GIF'
+     */
     public function prefix(): string
     {
         return match ($this) {
@@ -21,6 +28,14 @@ enum GoodCodeType
         };
     }
 
+    /**
+     * Get GoodCodeType by code.
+     *
+     * @param  string  $code  The good code
+     * @return GoodCodeType The method returns the `GoodCodeType` object
+     *
+     * @example GoodCodeType::of('OPT2231433') => GoodCodeType::OPTION
+     */
     public static function of(string $code): GoodCodeType
     {
         $prefix = strtoupper(substr($code, 0, 3));
